ddc4ce233f5a8feb07ccd3d71bb31a9dff60d673
didko1 <36666629+didko1@users.noreply.github.com>
2018-03-17 17:04:11 +0200

    Update homework golden ratio.py

--- homework golden ratio.py
+++ homework golden ratio.py
@@ -1,8 +1,1 @@
-n = int(input("enter number "))
-for a in range(1, 100):
-    for b in range(1, 100):
-        c = a+b
-        if  (a/b) == (b/c) and a/b <= 0.1:
-            print("yes")
-       #else:
-       #    print("no golden ratio")
+
